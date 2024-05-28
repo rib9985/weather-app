7d538c505d7df1dba92a37f0fe55a34b7287d40e
rib9985 <rib9985@gmail.com>
2024-05-28 17:48:38 -0300

    Refactor to async await, change style.css

--- src/fetchImage.js
+++ src/fetchImage.js
@@ -1,19 +1,16 @@
-export default function fetchImage(searchImageText) {
-	const img = document.querySelector('img');
-	fetch(
-		`https://api.giphy.com/v1/gifs/translate?api_key=h2qohzuFA88jV1yCN3EidYUeOPbJAwLj&s=${searchImageText}`,
-		{
-			mode: 'cors',
-		},
-	)
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (response) {
-			img.src = response.data.images.original.url;
-		})
-		.catch(() => {
-			console.error('No gifs found!');
-			alert('No gifs found');
-		});
+export default async function fetchImage(searchImageText) {
+	try {
+		const img = document.querySelector('img');
+		const response = await fetch(
+			`https://api.giphy.com/v1/gifs/translate?api_key=h2qohzuFA88jV1yCN3EidYUeOPbJAwLj&s=${searchImageText}`,
+			{
+				mode: 'cors',
+			},
+		);
+		const imageData = await response.json();
+		img.src = imageData.data.images.original.url;
+	} catch (error) {
+		alert(`No gifs found! Error: ${error.message}`);
+		console.error(error);
+	}
 }
